87eb2b59e2f3c0694d116591b25695146a5a8677
zhangyuang <leonin0829@gmail.com>
2022-01-09 19:13:05 +0800

    chore: update example

--- react-dynamic-router-test/web/pages/detail/fetch.ts
+++ react-dynamic-router-test/web/pages/detail/fetch.ts
@@ -6,7 +6,6 @@ const fetch: ReactFetch<{
     index: (id: string) => Promise<Ddata>
   }
 }, {id: string}> = async ({ ctx, routerProps }) => {
-  console.log('xxx', routerProps)
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
   const data = __isBrowser__ ? await (await window.fetch(`/api/detail/${routerProps!.match.params.id}`)).json() : await ctx!.apiDeatilservice.index(ctx!.params.id)
   return {
